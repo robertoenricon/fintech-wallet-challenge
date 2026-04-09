bec29764dbcba8b3635f19c2619a12648879d2d1
Roberto Enrico <robertoenrico_@hotmail.com>
2026-04-08 23:36:04 -0300

    Criando Carteira de usuários

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,4 +20,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
     ];
+
+    public function wallet()
+    {
+        return $this->hasOne(Wallet::class);
+    }
+    
 }
